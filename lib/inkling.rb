76f00bd63687940f4792d1574851f7c43171a7da
Brandon Robins <brandon@ourlabel.com>
2014-07-30 23:13:23 -0500

    Require xml parsers

--- lib/inkling.rb
+++ lib/inkling.rb
@@ -11,6 +11,11 @@ require "inkling/market"
 require "inkling/stock"
 require "inkling/price"
 
+require "inkling/parsers/base_parser"
+require "inkling/parsers/market_parser"
+require "inkling/parsers/price_parser"
+require "inkling/parsers/stock_parser"
+
 require "pry"
 
 Dir["./lib/inkling/parsers/*.rb"].each{|f| require f }
