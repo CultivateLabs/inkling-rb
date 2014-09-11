701fff5342c689dd9eb4d4e9da78555387dceb41
Jack Schultz <jackschultz23@gmail.com>
2014-09-11 13:09:26 -0500

    dynamically-ish generating subclass

--- lib/inkling/parsers/stat_parser.rb
+++ lib/inkling/parsers/stat_parser.rb
@@ -13,11 +13,11 @@ module Inkling
       def parse_stat_node(stat_node)
         attrs = stat_node.to_hash
         type = attrs[:kids].find{|kid| kid[:name] == "stat_type"}[:text]
-        if type == "quantity_position"
-          stat = Inkling::QuantityStat.new
-        elsif type == "percentage_position"
-          stat = Inkling::PercentageStat.new
-        end
+
+        underscore_descendants = Inkling::Stat.descendants.map{|s| s.to_s.split("::").last.underscore}
+        raise "Invalid Stat Type" unless underscore_descendants.include?(type)
+        stat = Inkling::Stat.descendants[underscore_descendants.index(type)].new
+
         attrs[:kids].each do |node|
           attr_name = node[:name].gsub("-", "_")
           stat.send("#{attr_name}=", parse_node_value(node)) if stat.respond_to?("#{attr_name}=")
