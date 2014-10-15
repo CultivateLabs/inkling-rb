1bb6ca80c04073d10f981e6bfc9dfb0ac0ce22ad
Jack Schultz <jackschultz23@gmail.com>
2014-10-15 10:53:40 -0500

    membership parser

---
+++ lib/inkling/parsers/membership_parser.rb
@@ -0,0 +1,33 @@
+module Inkling
+  module Parsers
+    class MembershipParser < BaseParser
+      def parse
+        memberships = []
+        xml_doc.xpath("//memberships").children.each do |membership_node|
+          memberships << parse_membership_node(membership_node)
+        end
+
+        memberships
+      end
+
+      def parse_membership_node(membership_node)
+        attrs = membership_node.to_hash
+        membership = Inkling::Membership.new
+        membership.type = attrs[:name]
+        attrs[:kids].each do |node|
+          if node[:name] == "user"
+            node[:kids].each do |unode|
+              attr_name = unode[:name].gsub("-", "_")
+              membership.send("#{attr_name}=", parse_node_value(unode)) if membership.respond_to?("#{attr_name}=")
+            end
+          else
+            attr_name = node[:name].gsub("-", "_")
+            membership.send("#{attr_name}=", parse_node_value(node)) if market.respond_to?("#{attr_name}=")
+          end
+        end
+
+        membership
+      end
+    end
+  end
+end
